26748da77da9070d1e590a75da3c49b878344456
Samy <mail@samynitsche.de>
2020-05-18 09:06:52 +0200

    Fix tests

--- tests/Unit/DeleteAuthLogTest.php
+++ tests/Unit/DeleteAuthLogTest.php
@@ -5,6 +5,7 @@ namespace SamuelNitsche\AuthLog\Tests\Unit;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Notification;
 use SamuelNitsche\AuthLog\Tests\TestCase;
 use SamuelNitsche\AuthLog\Tests\Models\User;
 
@@ -13,6 +14,8 @@ class DeleteAuthLogTest extends TestCase
     /** @test */
     public function it_deletes_all_entries_older_than_the_given_value()
     {
+        Notification::fake();
+        
         Carbon::setTestNow('2019-01-01 00:00:00');
 
         Auth::login($user = User::create(['name' => 'JohnDoe', 'email' => 'john@example.com']));
